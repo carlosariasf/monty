bfbf74509bef82dc2f9ef7607a1895f80e5efa4a
carlosariasf <carlosarias@icreativa.com.co>
2019-07-31 18:26:46 +0000

    rotl

--- rotl.c
+++ rotl.c
@@ -12,24 +12,27 @@ void rotl_m(stack_t **stack, unsigned int line_number)
 	stack_t *currentbot = NULL;
 	(void) line_number;
 	
-	currentbot = *stack;
-	while (currentbot->next)
-		currentbot = currentbot->next, i++;
-	currenttop = *stack;
-	if (i == 2)
+	if (*stack)
 	{
-		*stack = currenttop->next;
-		(*stack)->prev = NULL;
-		(*stack)->next = currenttop;
-		currenttop->prev = (*stack);
-		currenttop->next = NULL;
-	}
-	if (i > 2)
-	{	
-		*stack = currenttop->next;
-		(*stack)->prev = NULL;
-		currentbot->next = currenttop;
-		currenttop->next = NULL;
-		currenttop->prev = currentbot;
+		currentbot = *stack;
+		while (currentbot->next)
+			currentbot = currentbot->next, i++;
+		currenttop = *stack;
+		if (i == 2)
+		{
+			*stack = currenttop->next;
+			(*stack)->prev = NULL;
+			(*stack)->next = currenttop;
+			currenttop->prev = (*stack);
+			currenttop->next = NULL;
+		}
+		if (i > 2)
+		{	
+			*stack = currenttop->next;
+			(*stack)->prev = NULL;
+			currentbot->next = currenttop;
+			currenttop->next = NULL;
+			currenttop->prev = currentbot;
+		}
 	}
 }
